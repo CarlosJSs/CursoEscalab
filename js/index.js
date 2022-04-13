6c040dd260a82c07dfdd820c7f73e7f8d2eefe5b
CarlosJSs <carlos.0577@outlook.com>
2022-04-13 17:46:52 -0500

    Modificar el DOM creando nuevos elementos

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
-//----------------------Seleccionar y manipular elementos que ya tenemos en el DOM----------------------
-const enlaces = document.querySelectorAll('a');
+//----------------------Seleccionar y manipular elementos en el DOM----------------------
+const formulario = document.querySelector('#formulario-contacto');
+
+const botonLimpiar = document.createElement('button');
+const textoBotonLimpiar = document.createTextNode('Limpiar formulario');
+botonLimpiar.appendChild(textoBotonLimpiar);
+
+const botonEnviar = document.querySelector('.btn-enviar');
+
+botonEnviar.insertAdjacentElement('afterend' ,botonLimpiar);
+
+botonLimpiar.className = 'btns btn-enviar btn-limpiar';
+
+//formulario.innerHTML += '<button class="btns btn-enviar btn-limpiar">Limpiar formulario</button>';
+document.querySelector('.btn-limpiar').style.backgroundColor = '#ff0000';
+//formulario.removeChild(document.querySelector('.btn-limpiar'));
+
+//formulario.parentNode.removeChild(formulario);
 
 //conts enlaces = document.getElementsByTagName('a');
 
-Array.from(enlaces).forEach(element => {
-    element.style.backgroundColor = '#000000';    
-});
+//Array.from(enlaces).forEach(element => {
+    //element.parentNode.removeChild(element);
+    //element.style.backgroundColor = '#000000';    
+//});
 
 //console.log(enlaces);
 
