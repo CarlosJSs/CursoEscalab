b5d390ed3555a8f5f5dfa2cc0057e51c386bac9f
CarlosJSs <carlos.0577@outlook.com>
2022-04-13 16:59:34 -0500

    Modificar el DOM con elementos ya existentes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("Cargó JS");
+//----------------------Seleccionar y manipular elementos que ya tenemos en el DOM----------------------
+const enlaces = document.querySelectorAll('a');
+
+//conts enlaces = document.getElementsByTagName('a');
+
+Array.from(enlaces).forEach(element => {
+    element.style.backgroundColor = '#000000';    
+});
+
+//console.log(enlaces);
+
+//const formulario = document.getElementById('formulario-contacto');
+//formulario.style.fontFamily = 'serif';
+//formulario.style.width = '50px';
+
+//const botonVerServicios = document.querySelector('.btn-hero');
+//botonVerServicios.className += ' btn-enviar';
+//console.log(botonVerServicios.className);
+
+//botonVerServicios.style.backgroundColor = '#000000';
+//botonVerServicios.innerHTML = 'Cambió';
